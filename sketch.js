00caeccd6d2117fd6c09afee07746fe9bdd0c5db
ronikaufman <ronikaufman97@gmail.com>
2019-12-26 11:20:18 +0100

    base code

---
+++ sketch.js
@@ -0,0 +1,36 @@
+// By Roni Kaufman
+
+let randInt = (a, b) => (floor(random(a, b)));
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	//colorMode(HSB, 100);
+	noLoop();
+	noStroke();
+}
+
+function draw() {
+	let pixelSize = 20;
+	let pixWidth = width/pixelSize;
+	let pixHeight = height/pixelSize;
+	let nRectangles = floor(width*height)/1000;
+	let maxW = randInt(6, 10);
+	let maxH = randInt(6, 10);
+	
+	let alpha = 225;
+	let colors = [color(10, 10, 10, alpha),
+								color(247, 243, 242, alpha),
+								color(0, 119, 225, alpha),
+								color(245, 210, 22, alpha),
+								color(252, 53, 3, alpha)];
+	
+	background(colors[1]);
+	for (let i = 0; i < nRectangles; i++) {
+		fill(colors[floor(random(colors.length))]);
+		let x = randInt(0, pixWidth) * pixelSize;
+		let y = randInt(0, pixHeight) * pixelSize;
+		let w = randInt(1, maxW) * pixelSize;
+		let h = randInt(1, maxH) * pixelSize;
+		rect(x, y, w, h);
+	}
+}
